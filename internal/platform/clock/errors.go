bf90d8bce6a8bf7fd4e98e2e11900086e53d2fcb
Proxynth <proxynth.tech@gmail.com>
2026-08-23 23:38:30 +0200

    PS-028 - Implement time advancement (#28)
    Implement explicit virtual time advancement.

--- internal/platform/clock/errors.go
+++ internal/platform/clock/errors.go
@@ -3,6 +3,7 @@ package clock
 import "errors"
 
 var (
+	ErrInvalidClock    = errors.New("invalid clock")
 	ErrInvalidTime     = errors.New("invalid clock time")
 	ErrInvalidAdvance  = errors.New("invalid clock advance")
 	ErrBackwardAdvance = errors.New("clock cannot move backwards")
